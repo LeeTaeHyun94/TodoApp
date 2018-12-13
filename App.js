0620f5b389b957e16b9af7b63519da35b1773d79
Tae Hyun Lee <leethw@naver.com>
2018-12-13 21:35:06 +0900

    Create Main page & Todo component / Date:20181213

--- App.js
+++ App.js
@@ -7,34 +7,49 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, StatusBar, TextInput, Dimensions, ScrollView} from 'react-native';
+import Todo from './components/Todo';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const {height, width} = Dimensions.get("window");
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    newTodo: ""
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <StatusBar barStyle="light-content"/>
+          <Text style={styles.title}>To Do</Text>
+          <View style={styles.card}>
+            <TextInput style={styles.input} placeholder={"New To Do"} value={this.state.newTodo} onChangeText={this._controlNewTodo} placeholderTextColor={"#999"} returnKeyType={"done"} autoCorrect={false}/>
+            <ScrollView>
+              <Todo/>
+            </ScrollView>
+          </View>
       </View>
     );
   }
+  _controlNewTodo = text => {
+    this.setState({
+        newTodo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F23657',
   },
   welcome: {
     fontSize: 20,
@@ -46,4 +61,40 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+    title: {
+      color: "white",
+        fontSize: 30,
+        marginTop: 50,
+        marginBottom: 50,
+        fontWeight: "100"
+    },
+    card: {
+      backgroundColor: "white",
+        flex: 1,
+        width: width - 25,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        // ios : shadowRadius
+        // android : elevation
+        ...Platform.select({
+            ios: {
+              shadowColor: "rgb(50, 50, 50)",
+                shadowOpacity: 0.5,
+                shadowRadius: 5,
+                shadowOffset: {
+                  height:-1,
+                    width:1
+                }
+            },
+            android: {
+              elevation: 3
+            }
+        })
+    },
+    input: {
+      padding: 20,
+      borderBottomColor: "#bbb",
+      borderBottomWidth: 1,
+        fontSize: 25,
+    },
 });
---
+++ components/Todo.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
+
+export default class Todo extends React.Component{
+    render() {
+        return (
+            <View>
+                <Text>Hello I'm a To Do.</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
